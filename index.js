8cab9bb3557385273ab2dc98ef3f513f732709b0
erickcmen <erickcmend@gmail.com>
2020-09-08 23:41:53 -0500

    Agregando archivos

---
+++ index.js
@@ -0,0 +1,33 @@
+const TelegramBot = require( "node-telegram-bot-api" );
+const token = '12345678910:sdfasiwueiwueiuwehfuiwe'
+const bot = new TelegramBot(token, {polling: true});
+bot.onText(/\/start/, (msg) => {
+bot.sendMessage(msg.chat.id, "Bienvenido", {
+"reply_markup": {
+    "keyboard": [["hola", "bye"], ["video","musica"]]
+    }
+});
+});
+//Respuestas
+bot.on('message', (msg) => {
+var Hi = "hola";
+if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id, "Hola");
+    bot.sendPhoto(msg.chat.id,"img/coronavirus.png");
+}
+var bye = "bye";
+if (msg.text.toString().toLowerCase().includes(bye)) {
+    bot.sendMessage(msg.chat.id, "Bye amiguito");
+}
+var vid = "video";
+if (msg.text.indexOf(vid) === 0) {
+    bot.sendMessage(msg.chat.id, "Coronavairus!!!");
+    bot.sendVideo(msg.chat.id,"img/Coronavairus.mp4")
+}
+var mus= "musica";
+if (msg.text.indexOf(mus) === 0) {
+    bot.sendMessage(msg.chat.id, "Ahora baila este rico cumbión");
+    bot.sendAudio(msg.chat.id,"img/Coronavairus.mp3")
+}
+});
+
